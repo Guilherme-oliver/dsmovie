3f4174d3720b6ed53840a85a119ac7dea0d990a3
Guilherme Oliveira <guilherme.rodriguess.oliveira@gmail.com>
2022-03-12 15:45:00 -0300

    correction

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,11 +7,12 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/Guilherme-oliver"></a>
-          <div className="dsmovie-contact-container">
-            <GithubIcon />
-            <p className="dsmovie-contact-link">/devsuperior</p>
-          </div>
+          <a href="https://github.com/Guilherme-oliver">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/devsuperior</p>
+            </div>
+          </a>
         </div>
       </nav>
     </header>
